3acbea4ac060c2d47ece4e0e5c1ff1b19726d5b8
yeshwanthchauhan <yeshwanthchauhan2000@gmail.com>
2024-12-24 14:47:43 +0900

    un cmt 1st chunk and uncmt calculator

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 // DSA C++
 
-#include <iostream>
-using namespace std;
-int main() {
-    int age = 24; //4 bytes
-    string name = "Yeshwanth Chauhan"; // 1 byte per character
-    float PI = 3.14f; //4 bytes
-    bool isSafe = true; //1 byte
-    double price = 100.50; //8 bytes
-    cout << "My name is " << name << " and I am " << age << " years old .\n" ;
-    cout << (PI) << "\n";
-    cout << "Below iss the size of name and age var \n";
-    cout <<  sizeof(name) << endl;   //Gives the size of the string 
-    cout << sizeof(age) << endl;    // Gives the size of the integer
-    return 0;
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int age = 24; //4 bytes
+//     string name = "Yeshwanth Chauhan"; // 1 byte per character
+//     float PI = 3.14f; //4 bytes
+//     bool isSafe = true; //1 byte
+//     double price = 100.50; //8 bytes
+//     cout << "My name is " << name << " and I am " << age << " years old .\n" ;
+//     cout << (PI) << "\n";
+//     cout << "Below iss the size of name and age var \n";
+//     cout <<  sizeof(name) << endl;   //Gives the size of the string 
+//     cout << sizeof(age) << endl;    // Gives the size of the integer
+//     return 0;
 
-}
+// }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -69,42 +69,42 @@ int main() {
 
 //simple calculator in C++
 
-// #include <iostream>
-// using namespace std;
-// int main() {
+#include <iostream>
+using namespace std;
+int main() {
 
-// int a = 0;
-// int b = 0;
-// int c = 0;
-// cout << "Enter the first number : ";
-// cin >> b;
-// cout << "Enter the second number : ";
-// cin >> c;
-// cout << "what do you want to do with the numbers?" << endl;
-// cout << "1. Addition" << endl;
-// cout << "2. Subtraction" << endl;
-// cout << "3. Multiplication" << endl;
-// cout << "4. Division" << endl;
-// cout << "5. Modulus" << endl;
-// int choice;
-// cin >> choice;
-// switch (choice) { 
-//     case 1: a = b + c;
-//     break;
-//     case 2: a = b - c;
-//     break;
-//     case 3: a = b * c;
-//     break;
-//     case 4: a = b / c;
-//     break;
-//     case 5: a = b % c;
-//     break;
-//     default: cout << "Invalid choice";
-//     }
-//     cout << "The result is : " << a << endl;
-//     return 0;
+int a = 0;
+int b = 0;
+int c = 0;
+cout << "Enter the first number : ";
+cin >> b;
+cout << "Enter the second number : ";
+cin >> c;
+cout << "what do you want to do with the numbers?" << endl;
+cout << "1. Addition" << endl;
+cout << "2. Subtraction" << endl;
+cout << "3. Multiplication" << endl;
+cout << "4. Division" << endl;
+cout << "5. Modulus" << endl;
+int choice;
+cin >> choice;
+switch (choice) { 
+    case 1: a = b + c;
+    break;
+    case 2: a = b - c;
+    break;
+    case 3: a = b * c;
+    break;
+    case 4: a = b / c;
+    break;
+    case 5: a = b % c;
+    break;
+    default: cout << "Invalid choice";
+    }
+    cout << "The result is : " << a << endl;
+    return 0;
     
-// }
+}
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
