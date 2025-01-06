96538765c7ee7a08e930276ee96d082eee575506
yeshwanthchauhan <yeshwanthchauhan2000@gmail.com>
2025-01-06 14:16:48 +0900

    Contsructors

--- oops.cpp
+++ oops.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello from oops.cpp!" << std::endl;
+using std::string;
+
+class Employee {
+    public:
+        string Name;
+        string Company;
+        int Age;
+
+    void IntroduceYourself() {
+        std::cout<< "Name - " << Name <<std::endl;
+        std::cout<< "Company - " << Company <<std::endl;
+        std::cout<<"Age - " << Age << std::endl;
+    }
+    Employee(string name, string company, int age) {
+        Name = name;
+        Company = company;
+        Age = age;
+    }
+};
+
+
+int main(){
+    Employee employee1 = Employee( "Yeshwanth", "Komatsu", 24);
+    employee1.IntroduceYourself();
+
+    Employee employee2 = Employee("Satani", "Komatsu", 25);
+    employee2.IntroduceYourself();
     return 0;
 }
